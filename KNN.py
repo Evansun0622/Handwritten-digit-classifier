bef116b8104ccdbeec4896894b860cf7df99d3f6
Evan Sun <46755542+Evansun0622@users.noreply.github.com>
2020-02-14 19:22:48 -0500

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,68 @@
+﻿'''
+kNN: k Nearest Neighbors
+
+Input:      inX: vector to compare to existing dataset (1xN) 
+            dataSet: size m data set of known vectors (NxM)
+            labels: data set labels (1xM vector)
+            k: number of neighbors to use for comparison
+            
+Output:     the number as which the digit was classified
+
+@author: Evan Sun
+'''
+import numpy as np
+import operator
+from os import listdir
+
+#KNN classifier
+def classify0(inX, dataSet, labels, k): 
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize,1)) - dataSet 
+    sqDiffMat = diffMat**2 
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()     
+    classCount={}          
+    for i in range(k): 
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def img2vector(filename):
+    returnVect = np.zeros((1,1024)) 
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')  
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m,1024))
+    #range(m)
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+        
+    testFileList = listdir('testDigits')        
+    errorCount = 0.0    
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i] 
+        fileStr = fileNameStr.split('.')[0]     
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print ("\nthe total number of errors is: %d" % errorCount)
+    print( "\nthe total accuracy rate is: %f" % (1.0-errorCount/float(mTest)))
+handwritingClassTest()
